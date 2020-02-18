e32204e82c49047536e7bc56a87aa8ccc2a39d8b
Student <student@Makerss-MacBook-Air.local>
2020-02-18 15:30:05 +0000

    raises an error when the airoprt id full

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,16 +3,25 @@
 describe('Airport', function() {
   var airport
   var plane
+
   beforeEach(function() {
     airport = new Airport();
     plane = new Plane();
   });
+
   it ('can keep track of planes', function(){
     airport.land(plane)
     expect(airport.hangar).toContain(plane)
   });
+  
   it ('can take off', function(){
     airport.takeOff(plane)
     expect(airport.hangar).toEqual([])
+  });
+
+  it ('raises error when airport is full', function(){
+    airport.land(plane)
+    airport.land(plane)
+    expect(function(){ airport.land(plane) }).toThrowError("Airport is full")
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,19 +1,30 @@
 'use strict';
+const CAPACITY = 2
 
 function Airport() {
     this.hangar = []
+    
 }; 
 // Airport.prototype.planes = function(){return []; };
 Airport.prototype.land = function(plane) {
-    this.hangar.push(plane)
+    if (this.full()) {
+        throw new Error ("Airport is full");
+    } else {
+        this.hangar.push(plane)
+    };
 };
 
 Airport.prototype.takeOff = function(plane){
     this.hangar.pop(plane)
 };
 
+Airport.prototype.full = function(){
+    return this.hangar.length >= CAPACITY
+};
+
 var airport = new Airport;
 var plane = new Plane;
 
 console.log(airport.land(plane));
-console.log(airport.hangar)
+console.log(airport.hangar.length)
+console.log(airport.full)
